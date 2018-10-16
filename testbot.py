e447d90f1aa72bca521097ab1c97e9341cc7eec0
voonfoo98 <voonfoo98@users.noreply.github.com>
2018-10-17 01:53:16 +0800

    simple working /createloan function

--- testbot.py
+++ testbot.py
@@ -6,22 +6,41 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 bot_token = "674824175:AAE4Rtk7nhLfq78juNPq9GT80UC_pB7W8oM"
-
 bot = telebot.TeleBot(bot_token)
 
 # use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
+spreadsheet = client.open("Sheares Media Inventory")
+
+# Assign each sheet to a var
+equip_sheet = spreadsheet.worksheet("Equipment_list")
+current_loan = spreadsheet.worksheet("Loan")
 
 # simple start command & bot responds with querying from google sheets
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    sheet = client.open("Sheares Media Inventory").sheet1
-    sheet_msg = sheet.row_values(1)
-    bot.reply_to(message, sheet_msg)
+    bot.reply_to(message, "Hi! You can create a loan by /createloan")
+
+@bot.message_handler(commands=['createloan'])
+def create_loan(message):
+    msg = bot.reply_to(message, "-----New Loan Entry-----\n\n" + "Name: \n" + "Block: \n" + "Item: \n" + "Date: \n" + "Duration: \n" + "Purpose: \n\n" +
+                       "*****Split each entry with SPACE *****" )
+    bot.register_next_step_handler(msg, process_name_step)
+    
+
+def process_name_step(message):
+    try:
+        user_data = message.text.split()
+        current_loan.append_row(user_data)
+        reply_message = "Loan created successfully!\n\n" + "Name: {}\n" + "Block: {}\n" + "Item: {}\n" + "Date: {}\n" + "Duration: {}\n" + "Purpose: {}\n\n"
+        msg = reply_message.format(user_data[0], user_data[1], user_data[2], user_data[3], user_data[4], user_data[5])
+        bot.reply_to(message, msg)
+    except:
+        bot.reply_to(message, "Sorry. Loan has failed to create.")
+
 
 def main_loop():
     bot.polling(True)
